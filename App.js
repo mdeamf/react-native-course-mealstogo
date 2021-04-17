d5eca238aeeadcb5ea477d1d007f925073519517
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-17 00:03:58 -0300

    feat: changing the main app layout

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchWrapper}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listWrapper}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  searchWrapper: {
+    padding: 8,
+    width: 'auto',
+    backgroundColor: 'purple',
+  },
+  listWrapper: {
+    flex: 1,
+    flexGrow: 1,
+    padding: 8,
+    backgroundColor: 'green',
   },
 });
