46c039825c064b9633ca01722f65cb09807c0308
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-17 19:32:02 -0300

    feat: adding oswald and lato fonts

--- App.js
+++ App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
+import { useFonts as useOswald, Lato_400Regular } from '@expo-google-fonts/lato';
+import { useFonts as useLato, Oswald_400Regular } from '@expo-google-fonts/oswald';
+
 import { theme } from './src/styles/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen';
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
