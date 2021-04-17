bab9070bf7e10e0369ec8b20e0bbdc438d588a58
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-17 02:02:52 -0300

    feat: adding theme

---
+++ src/styles/theme.js
@@ -0,0 +1,84 @@
+const sizes = {
+  sm  : '8px',
+  md  : '16px',
+  lg  : '32px',
+  xl  : '64px',
+  xxl : '128px',
+};
+
+const lineHeights = {
+  title : '28px',
+  copy  : '20px',
+};
+
+const space = {
+  zero : '0px',
+  sm   : '4px',
+  md   : '8px',
+  lg   : '16px',
+  xl   : '32px',
+  xxl  : '64px',
+};
+
+const fonts = {
+  body      : 'Oswald_400Regular',
+  heading   : 'Lato_400Regular',
+  monospace : 'Oswald_400Regular',
+};
+
+const fontWeights = {
+  regular : 400,
+  medium  : 500,
+  bold    : 700,
+};
+
+const fontSizes = {
+  caption : '12px',
+  button  : '14px',
+  body    : '16px',
+  title   : '20px',
+  h5      : '24px',
+  h4      : '34px',
+  h3      : '45px',
+  h2      : '56px',
+  h1      : '112px',
+};
+
+const colors = {
+  brand: {
+    primary   : '#2182BD',
+    secondary : '#5282BD',
+    muted     : '#C6DAF7',
+  },
+  ui: {
+    primary    : '#262626',
+    secondary  : '#757575',
+    tertiary   : '#F1F1F1',
+    quaternary : '#FFFFFF',
+    disabled   : '#DEDEDE',
+    error      : '#D0421B',
+    success    : '#138000',
+  },
+  bg: {
+    primary   : '#FFFFFF',
+    secondary : '#F1F1F1',
+  },
+  text: {
+    primary   : '#262626',
+    secondary : '#757575',
+    disabled  : '#9C9C9C',
+    inverse   : '#FFFFFF',
+    error     : '#D0421B',
+    success   : '#138000',
+  },
+};
+
+export const theme = {
+  sizes,
+  lineHeights,
+  space,
+  fonts,
+  fontWeights,
+  fontSizes,
+  colors,
+};
