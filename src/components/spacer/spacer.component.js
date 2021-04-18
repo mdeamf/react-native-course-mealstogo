7987641bb16bb6f45352149afc5fe89b9b59b02d
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-18 13:50:36 -0300

    refactor: removing unused logs

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -4,13 +4,9 @@ import styled from 'styled-components/native';
 export const Spacer = ({ variant }) => {
   const [direction, size] = variant.split('.');
 
-  console.log(variant, direction, size);
-
   const SpacerVariant = styled.View`
     margin-${direction}: ${(props) => props.theme.space[size]};
   `;
-
-  console.log(SpacerVariant);
 
   return <SpacerVariant />;
 };
