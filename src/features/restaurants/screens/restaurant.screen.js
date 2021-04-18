1341f84a832ab83cead83821c66bb10e2b2554c0
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-18 13:50:51 -0300

    fix: android status bar value

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -7,7 +7,7 @@ import { RestaurantInfoCard } from '../components/restaurant-info-card.component
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   background-color: ${props => props.theme.colors.bg.secondary};
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight} px`};
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled(View)`
