697fa82a66d50b39d5f2ee46cf08d40f402b4089
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-18 18:22:41 -0300

    feat: integrating an initial list of restaurants

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import { FlatList, SafeAreaView, StatusBar } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${props => props.theme.colors.bg.secondary};
+  background-color: ${(props) => props.theme.colors.bg.secondary};
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
-const SearchContainer = styled(View)`
-  padding: ${props => props.theme.space.md};
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space.md};
 `;
 
-const ListContainer = styled(View)`
-  flex: 1;
-  flex-grow: 1;
-  padding: ${props => props.theme.space.md};
-`;
+const ListContainer = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => {
   return (
@@ -26,10 +26,13 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
-      <ListContainer>
-        <RestaurantInfoCard />
-      </ListContainer>
+
+      <ListContainer
+        data={[{ name: 'Test 1' }, { name: 'Test 2' }, { name: 'Test 3' }]}
+        renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
-
