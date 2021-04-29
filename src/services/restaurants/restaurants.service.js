7d804e3986a7410a77900150e266dfefe1ce1e91
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2021-04-29 17:11:22 -0300

    feat: adding mocked images

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,5 +1,5 @@
 import camelize from 'camelize';
-import { mocks } from './mock';
+import { mockImages, mocks } from './mock';
 
 export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
   return new Promise((resolve, reject) => {
@@ -13,7 +13,10 @@ export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
 
 export const restaurantsMap = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    delete restaurant.photos;
+    restaurant.photos = restaurant.photos.map((p) => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    })
+
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours?.open_now,
